1edd864ffc249629750384d795ce35780f95cf48
Aliasger Mala <aliasgerzmala@gmail.com>
2025-10-02 16:15:41 +0300

    Changes

--- server.js
+++ server.js
@@ -1,25 +1,3 @@
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import authRoutes from './routes/authRoutes.js';
-// import userRoutes from './routes/userRoutes.js';
-
-// dotenv.config();
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use('/api', authRoutes);
-// app.use('/api/users', userRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// app.use((req, res, next) => {
-//   console.log(`[${req.method}] ${req.originalUrl}`);
-//   next();
-// });
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -42,5 +20,5 @@ app.use((req, res, next) => {
 app.use('/api', authRoutes);
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
