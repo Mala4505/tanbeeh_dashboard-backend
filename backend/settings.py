6c588d6bea99b1fee8cd69a07ce8a13e142b9201
26365 Aliasger Mala <26365@mamal.nbi>
2025-10-21 15:23:13 +0300

    Ready for Render deployment with Procfile

--- backend/settings.py
+++ backend/settings.py
@@ -8,7 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-for-dev')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
